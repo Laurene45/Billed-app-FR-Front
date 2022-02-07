6e8039f0b0720aff03dece46184b73925f13ac05
Laurene COURDE <laurene.courde@gmail.com>
2022-02-07 14:04:37 +0100

    debug report - test Bills ordre NDF

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -30,8 +30,10 @@ describe("Given I am connected as an employee", () => {
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML);
+      // en erreur  : const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      // changement du calcul 
+      const antiChrono = (a, b) => b.date - a.date
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
